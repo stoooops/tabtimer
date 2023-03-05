43d13aea08f5ec07fd2042f41e5929f2ef532415
Cory Gabrielsen <corygabrielsen@gmail.com>
2023-03-05 01:28:31 -0800

    Add reload popup

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const reloadBtn = document.querySelector('#reload-btn');
+
+    reloadBtn.addEventListener('click', () => {
+        chrome.runtime.reload();
+    });
+});
